f81ca51e56cec4c823ccf95f2f6e42e8eb2439e9
danielmena4 <daniel__mena@hotmail.com>
2025-04-28 08:05:44 -0500

    se añadarieron data annotations

--- Evaluacion1erProgreso/Models/Cliente.cs
+++ Evaluacion1erProgreso/Models/Cliente.cs
@@ -1,16 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace Evaluacion1erProgreso.Models
 {
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(50)]
         public string Nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Range(-500, 500)]
         public decimal Saldo { get; set; }
+        [Required]
         public bool Activo { get; set; }
         public DateTime FechaCreacion { get; set; }
-        public List<Reserva> reservas { get; set; }
+        public List<Reserva> Reservas { get; set; }
         public PlanDeRecompensas PlanDeRecompensas;
     }
 }
--- Evaluacion1erProgreso/Models/PlanDeRecompensas.cs
+++ Evaluacion1erProgreso/Models/PlanDeRecompensas.cs
@@ -2,5 +2,10 @@
 {
     public class PlanDeRecompensas
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int puntos { get; set; } 
+        public string TipoDeRecompensa { get; set; } 
     }
 }
